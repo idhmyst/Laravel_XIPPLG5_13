0e2017df92fab2e5a04ad25ef5e6bff4a333da6b
idhmyst <baihaqiidham67@gmail.com>
2025-10-30 13:27:49 +0700

    Add Feature Read Student

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,7 +27,7 @@
   <!-- Sidebar -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="#" class="brand-link">
-      <img src="{{ asset('public/images/logo.png') }}" alt="E-cloud Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+     <img src="{{ asset('images/logo.png') }}" alt="E-cloud Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">E-Cloud</span>
     </a>
 
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StudentController;
 
 // Route utama (Landing Page)
 Route::get('/', [LandingController::class, 'index'])->name('landing');
 
 
+
 // Route group untuk admin
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+
+       Route::resource('students', StudentController::class);
 });
